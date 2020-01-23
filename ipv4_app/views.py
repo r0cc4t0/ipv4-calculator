1781df5562491215a53a4a1f12f5738bb4a49ee7
r0cc4t0 <guilherme.roccato@outlook.com>
2020-01-23 13:38:16 -0300

    Subnet Mask and Wildcard Mask

--- ipv4_app/views.py
+++ ipv4_app/views.py
@@ -50,6 +50,46 @@ def ip_type(ip):
     return ip_type
 
 
+def bin_octets(ip_mask):
+    octets = split_octets(ip_mask)
+    octet1 = dec_to_bin(int(octets[0])).rjust(8, '0')
+    octet2 = dec_to_bin(int(octets[1])).rjust(8, '0')
+    octet3 = dec_to_bin(int(octets[2])).rjust(8, '0')
+    octet4 = dec_to_bin(int(octets[3])).rjust(8, '0')
+    ip_mask = octet1 + '.' + octet2 + '.' + octet3 + '.' + octet4
+    return ip_mask
+
+
+def dec_octets(ip_mask):
+    octets = split_octets(ip_mask)
+    octet1 = str(bin_to_dec(octets[0]))
+    octet2 = str(bin_to_dec(octets[1]))
+    octet3 = str(bin_to_dec(octets[2]))
+    octet4 = str(bin_to_dec(octets[3]))
+    ip_mask = octet1 + '.' + octet2 + '.' + octet3 + '.' + octet4
+    return ip_mask
+
+
+def dec_mask(ip_class):
+    if ip_class == 'A':
+        dec_mask = '255.0.0.0'
+    elif ip_class == 'B':
+        dec_mask = '255.255.0.0'
+    elif ip_class == 'C':
+        dec_mask = '255.255.255.0'
+    return dec_mask
+
+
+def dec_wildcard(ip_class):
+    if ip_class == 'A':
+        dec_wildcard = '0.255.255.255'
+    if ip_class == 'B':
+        dec_wildcard = '0.255.255.255'
+    if ip_class == 'C':
+        dec_wildcard = '0.255.255.255'
+    return dec_wildcard
+
+
 data = {}
 
 
@@ -70,4 +110,9 @@ def result(request):
         data['inv_opt'] = 'hosts'
     data['class'] = ip_class(data['ip'])
     data['type'] = ip_type(data['ip'])
+    data['bin_ip'] = bin_octets(data['ip'])
+    data['dec_mask'] = dec_mask(data['class'])
+    data['bin_mask'] = bin_octets(data['dec_mask'])
+    data['dec_wildcard'] = dec_wildcard(data['class'])
+    data['bin_wildcard'] = bin_octets(data['dec_wildcard'])
     return render(request, 'result.html', data)
